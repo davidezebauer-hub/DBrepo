f42f62c7698ad1e38623d90d1edb386f087cc47c
davidezebauer-hub <david_eze_bauer@hotmail.com>
2019-06-20 15:30:53 -0300

    agregado de seleccionar fecha en ingresos

--- app/src/main/java/com/example/asistentedeahorro/DatePickerFragment.java
+++ app/src/main/java/com/example/asistentedeahorro/DatePickerFragment.java
@@ -3,22 +3,37 @@ package com.example.asistentedeahorro;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.widget.DatePicker;
 
 import java.util.Calendar;
 
-public class DatePickerFragment extends DialogFragment
-    implements DatePickerDialog.OnDateSetListener {
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState){
-        final Calendar c=Calendar.getInstance();
-        int anio=c.get(Calendar.YEAR);
-        int mes=c.get(Calendar.MONTH)+1;
-        int dia=c.get(Calendar.DAY_OF_MONTH);
-        return new DatePickerDialog(getActivity(),this,anio,mes,dia);
+public class DatePickerFragment extends DialogFragment {
+    DatePickerDialog.OnDateSetListener listener;
+    public static DatePickerFragment newInstance(DatePickerDialog.OnDateSetListener listener){
+    DatePickerFragment fragment=new DatePickerFragment();
+    fragment.setListener(listener);
+    return fragment;
     }
+
     @Override
+    @NonNull
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), listener, year, month, day);
+    }
+
+
+    public void setListener(DatePickerDialog.OnDateSetListener listener) {
+        this.listener=listener;
+    }
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
 
     }
--- app/src/main/java/com/example/asistentedeahorro/Ingresos.java
+++ app/src/main/java/com/example/asistentedeahorro/Ingresos.java
@@ -1,31 +1,63 @@
 package com.example.asistentedeahorro;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.Time;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.CalendarView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.DatePicker;
 
 public class Ingresos extends AppCompatActivity {
-private TextView monto;
+private TextView monto,fecha_selec;
 private Spinner concepto;
+private DatePicker datePicker;
 private int dia, mes, anio;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingresos);
+
         monto=(TextView)findViewById(R.id.monto);
+
         concepto=(Spinner)findViewById(R.id.sp_concepto);
         ArrayAdapter<CharSequence>adap=ArrayAdapter.createFromResource(this,R.array.conceptoI,
                 android.R.layout.simple_spinner_item);
         concepto.setAdapter(adap);
+
+        fecha_selec=(TextView)findViewById(R.id.fecha_selec);
+        Time hoy =new Time(Time.getCurrentTimezone());
+        hoy.setToNow();
+        dia=hoy.monthDay;
+        mes=hoy.month;
+        anio=hoy.year;
+        mostrar_fecha(dia,mes,anio);
+
     }
+
+    public void mostrar_fecha(int day, int month, int year) {
+        fecha_selec.setText(dosDig(day)+"/"+dosDig(month+1)+"/"+year);
+    }
+
+    private String dosDig(int n) {
+        return (n<=9) ? ("0"+n) : String.valueOf(n);
+    }
+
     public void abrir_calendario(View view){
-        DatePickerFragment newFragment=new DatePickerFragment();
-        newFragment.show(getSupportFragmentManager(),"datePicker");
+        DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                dia=day;
+                mes=month;
+                anio=year;
+                mostrar_fecha(dia,mes,anio);
+            }
+        });
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
     public void act_principal(View view){
         Intent i=new Intent(this, MainActivity.class);
